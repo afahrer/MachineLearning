d2fef45dcffde7b65f77dec3abb32de028809add
afahrer <Stamkos91>
2019-04-01 20:43:37 -0400

    add student data to repo

--- Youtube Tutorial/test.py
+++ Youtube Tutorial/test.py
@@ -1,10 +1,7 @@
-import tensorflow
-import keras
-import sklearn
-import pandas as pd
 import numpy as np
+import pandas as pd
+import sklearn
 from sklearn import linear_model
-from sklearn.utils import shuffle
 
 data = pd.read_csv('student-mat.csv', sep=";")
 data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
@@ -13,17 +10,17 @@ predict = "G3"
 
 x = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x,y,test_size=0.1)
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
 linear = linear_model.LinearRegression()
 linear.fit(x_train, y_train)
-acc = linear.score(x_test, y_test)
-print(acc)
+#acc = linear.score(x_test, y_test)
+#print(acc)
 
-print(linear.coef_)
-print(linear.intercept_)
+#print(linear.coef_)
+#print(linear.intercept_)
 
 predictions = linear.predict(x_test)
 
 for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
+    print(predictions[x], y_test[x])
