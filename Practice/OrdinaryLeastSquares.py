9a18c68261a97b1bf99482babc4b3e94396d2937
afahrer <Stamkos91>
2019-04-06 13:57:16 -0400

    Practiced Ordinary Least Sqaures Formula

---
+++ Practice/OrdinaryLeastSquares.py
@@ -0,0 +1,47 @@
+X = [12,23,54,23,54,21,42]
+Y = [32,23,77,64,12,35,23]
+
+sumx, sumy, sum1, sum2 = 0, 0, 0, 0
+length = len(X)
+
+# Get the mean of x and y
+for i in range(0, length):
+    sumx += X[i]
+    sumy += Y[i]
+meanx = sumx / length
+meany = sumy / length
+
+for i in range(0, length):
+    sum1 += (X[i] - meanx) * (Y[i] - meany)
+    sum2 += (X[i] - meanx) ** 2
+
+# m is slope of the line
+m = sum1 / sum2
+# b is the Y Intercept
+b = meany - (m * meanx)
+prediction = []
+for i in range(0, length):
+    prediction.append(m * X[i] + b)
+
+print("\nmy results")
+print("Slope :", m, "\nIntercept", b)
+print("Predications\n", prediction)
+
+# Using LinearRegression from sklearn
+from sklearn.linear_model import LinearRegression
+model = LinearRegression()
+X1 = [[12],[23],[54],[23],[54],[21],[42]]
+model.fit(X1, Y)
+
+print("\nsklean results")
+print("Slope :", model.coef_, "\nIntercept", model.intercept_)
+print("Predications\n", model.predict(X1))
+
+
+
+
+
+
+
+
+
--- Udemy Course/Section_4/Linear Regression.py
+++ Udemy Course/Section_4/Linear Regression.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from resources import PercentDiff
+import tensorflow
 
 # Importing the dataset
 dataset = pd.read_csv('Salary_Data.csv')
